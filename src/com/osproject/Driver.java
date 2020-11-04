3df791e96b14af0270d0b6fd24a29c5a4498e78c
Bradley Donahue <donahue.bradley@gmail.com>
2020-11-04 12:00:24 -0500

    Stub driver out to get a feel for how itll work

--- src/com/osproject/Driver.java
+++ src/com/osproject/Driver.java
@@ -6,9 +6,29 @@ public class Driver {
 
     public static void main(String[] args) {
 
-        ArrayList<PCB> readyQ;
-        ArrayList<PCB> completedQ;
-        ArrayList<PCB> runningQ;
+        ArrayList<PCB> readyQ = new ArrayList<>();
+        ArrayList<PCB> completedQ = new ArrayList<>();
+        ArrayList<PCB> runningQ = new ArrayList<>();
+        Memory memory = new Memory();
+        Loader loader = new Loader(memory);
+        CPU cpu = new CPU(memory);
+
+        loader.load("datafile.txt", readyQ);
+
+        Scheduler scheduler = new Scheduler(memory, readyQ, runningQ, completedQ, true);
+        Dispatcher dispatcher = new Dispatcher(cpu, scheduler);
+
+        scheduler.schedulePrograms();
+
+        /**
+         * While there are still processes to run
+         *      scheduler.checkForCompletion()
+         *      dispatcher.dispatch();
+         *      cpu.execute();
+         */
+
+        dispatcher.dispatch();
+        cpu.execute();
 
     }
 }
