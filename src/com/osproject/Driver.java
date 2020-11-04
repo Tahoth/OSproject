e74fffa011bcd9c4843280c24ba3b3119916d32c
Bradley Donahue <donahue.bradley@gmail.com>
2020-11-04 16:37:58 -0500

    Create core dump

--- src/com/osproject/Driver.java
+++ src/com/osproject/Driver.java
@@ -1,5 +1,11 @@
 package com.osproject;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -41,11 +47,39 @@ public class Driver {
 //        } while (c != 'n');
         } while (completedQ.size() != 30);
 
-        // Test ram dump
-        for (int i = 0; i < 1024; i++) {
-            String output = String.format("%08x: %08x", i, memory.retrieveRam(i));
-            System.out.println(output);
+        for (int i = 0; i < completedQ.size(); i++) {
+            PCB b = completedQ.get(i);
+            System.out.println("Process " + b.getPid() + " ran for: " + (b.mets.jobFinish - b.mets.jobStart) + "ms");
         }
 
+        try {
+            generateCoreDump(completedQ, memory);
+        } catch (Exception e) {
+            // Handle this
+        }
+    }
+
+    static void generateCoreDump(ArrayList<PCB> completedQ, Memory memory) throws Exception {
+        String outfile = "coredump.txt";
+        File f = new File(outfile);
+        OutputStream out = new FileOutputStream(f);
+        for (int i = 0; i < completedQ.size(); i++) {
+            PCB p = completedQ.get(i);
+            out.write(String.format("// JOB %X %X %X\n", p.getPid(), p.getInstructionCount(), p.getPriority()).getBytes());
+            for (int j = 0; j < p.getTotalSize(); j++) {
+                if (j == p.getInstructionCount()) {
+                    out.write(String.format("// Data %X %X %X\n", p.getInputSize(), p.getOutputSize(), p.getTempSize()).getBytes());
+                }
+                String line = String.format("0x%08X\n", memory.retrieveDisk(p.memInfo.startOnDisk + j));
+                out.write(line.getBytes());
+            }
+            out.write("// END\n".getBytes());
+        }
+
+        out.write("\nRAM\n".getBytes());
+        for (int i = 0; i < 1024; i++) {
+            String line = String.format("0x%08X: 0x%08X\n", i, memory.retrieveRam(i));
+            out.write(line.getBytes());
+        }
     }
 }
